6f2e87fb92e6a8df6927799491914b56f33ca045
Ahmad Taruiq <ahmad.tariq2528@gmail.com>
2020-03-12 23:59:57 +0500

    Final DiceApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -13,26 +14,41 @@ void main() => runApp(
       ),
     );
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void DiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var LeftDiceNumber = 5;
-    var RightDiceNumber = 3;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
               child: FlatButton(
             onPressed: () {
-              print('left button got pressed');
+              setState(() {
+                DiceFace();
+              });
             },
-            child: Image.asset('images/dice$LeftDiceNumber.png'),
+            child: Image.asset('images/dice$leftDiceNumber.png'),
           )),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice$RightDiceNumber.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
-                print('Right button got pressed');
+                setState(() {
+                  DiceFace();
+                });
               },
             ),
           )
